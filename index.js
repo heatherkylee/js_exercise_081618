08bce6a442d5baad5817b2f9fafcd98868a30046
Heather Lee <heather.k.lee@gmail.com>
2018-08-16 21:25:20 -0400

    install numeral.js, convert index.js to main.js file

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var moment = require("moment");
 
 console.log("test");
 console.log(moment().add(1, 'days').calendar()); 
+
+var numeral = require('numeral');
+
+console.log("numeral");
+console.log(numeral('$10,000.00'));
